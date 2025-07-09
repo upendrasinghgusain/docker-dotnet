a87e13af5bd6ab11fda49b6168d0f779ac109a6e
Upendra Gusain <UGusain@cfcunderwriting.com>
2025-07-09 22:03:23 +0100

    add console app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Press 'q' to quit or any other key to continue...");
+                var input = Console.ReadKey(true);
+                if (input.KeyChar == 'q')
+                {
+                    break;
+                }
+                Console.WriteLine($"You pressed: {input.KeyChar}");
+            } while (true);
+        }
+    }
+}
